32f8e210480efe77633b02084039267b8081703b
andrew3875 <80907464+andrew3875@users.noreply.github.com>
2024-11-15 17:59:27 +0200

    First commit

---
+++ form_test.cy.js
@@ -0,0 +1,19 @@
+﻿describe('Тесты формы обратной связи', () => {
+
+    it('Тест-кейс 1', () => {
+        cy.visit('https://kontaktnaya-forma.testograf.ru');
+
+        cy.get('.question_107').find('input').type('Иван Иванов'); 
+        cy.get('.question_108').find('input').type('ivanov@gmail.com'); 
+        cy.get('.question_109').find('input').type('+79991234567'); 
+
+        cy.get('#downshift-0-toggle-button').click();
+        cy.get('.option').first().click();
+
+        cy.get('.question_110').type('Сервисом пользуюсь давно, все четко, профессионально и быстро');
+
+        cy.get('.action___d45ea1f1799f107d8ccf').click();
+
+        cy.contains('Благодарим за обращение!')
+    });
+});
