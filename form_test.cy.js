a226b2484240e94e1c667486e25dcba511df0332
andrew3875 <80907464+andrew3875@users.noreply.github.com>
2024-11-17 16:36:45 +0200

    Add comments

--- form_test.cy.js
+++ form_test.cy.js
@@ -1,19 +1,19 @@
-﻿describe('Тест формы обратной связи', () => {
+describe('Тест формы обратной связи', () => {
 
     it('Тест-кейс 1', () => {
-        cy.visit('https://kontaktnaya-forma.testograf.ru');
+        cy.visit('https://kontaktnaya-forma.testograf.ru'); //посещение страницы с формой
 
-        cy.get('.question_107').find('input').type('Иван Иванов'); 
-        cy.get('.question_108').find('input').type('ivanov@gmail.com'); 
-        cy.get('.question_109').find('input').type('+79991234567'); 
+        cy.get('.question_107').find('input').type('Иван Иванов'); //заполнение поля "Ваше имя"
+        cy.get('.question_108').find('input').type('ivanov@gmail.com'); //заполнение поля "E-mail"
+        cy.get('.question_109').find('input').type('+79991234567'); //заполнение поля "Телефон"
 
-        cy.get('#downshift-0-toggle-button').click();
-        cy.get('.option').first().click();
+        cy.get('#downshift-0-toggle-button').click(); //клик по кнопке раскрытия меню
+        cy.get('.option').first().click(); //клик по опции "Заказ"
 
-        cy.get('.question_110').type('Сервисом пользуюсь давно, все четко, профессионально и быстро');
+        cy.get('.question_110').type('Сервисом пользуюсь давно, все четко, профессионально и быстро'); //заполнение поля "Ваше сообщение" 
 
-        cy.get('.action___d45ea1f1799f107d8ccf').click();
+        cy.get('.action___d45ea1f1799f107d8ccf').click(); //клик по кнопке отправки
 
-        cy.contains('Благодарим за обращение!')
+        cy.contains('Благодарим за обращение!'); //проверка отображения сообщения
     });
 });
